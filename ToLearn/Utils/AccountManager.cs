0132b4d3e83056ab239a6da2b6299f0a9745f62b
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-05 19:40:45 +0430

    Create RequestMaker and AccountManager classes

---
+++ ToLearn/Utils/AccountManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using ToLearn.Forms.Account;
+using ToLearn.Models.Account;
+
+namespace ToLearn.Utils;
+
+public class AccountManager
+{
+    public static bool Login(string email, string password)
+    {
+        var request = new Request()
+        {
+            Email = email,
+            Password = password
+        };
+        var requestMaker = new RequestMaker();
+        var response = requestMaker.Post("login", request);
+        try
+        {
+            LoginResponse loginResponse = JsonSerializer.Deserialize<LoginResponse>(response);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+}
---
+++ ToLearn/Utils/RequestMaker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using ToLearn.Models.Account;
+
+namespace ToLearn.Utils;
+
+public class RequestMaker
+{
+    private readonly HttpClient _client;
+
+    public RequestMaker(User? currentUser = null)
+    {
+        _client = new HttpClient()
+        {
+            BaseAddress = new Uri("Https://localhost:7190/")
+        };
+        if (currentUser != null)
+        {
+            _client.DefaultRequestHeaders.Add("bearer", currentUser.Token);
+        }
+    }
+
+    public string Get(string path)
+    {
+        return _client.GetAsync(path).Result.Content
+            .ReadAsStringAsync().Result;
+    }
+
+    public string Post(string path, Object obj)
+    {
+        var response = _client.PostAsJsonAsync<Object>(path, obj);
+        return response.Result.Content.ReadAsStringAsync().Result;
+    }
+}
