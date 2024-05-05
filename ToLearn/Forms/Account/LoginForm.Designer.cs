cc79cf3fa5cf787edbb2612b33a43db6a5423d25
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-05 17:24:39 +0430

    Add basic functionality to Login form

--- ToLearn/Forms/Account/LoginForm.Designer.cs
+++ ToLearn/Forms/Account/LoginForm.Designer.cs
@@ -28,18 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            emailTextBox = new TextBox();
+            passwordTextBox = new TextBox();
+            loginButton = new Button();
+            cancelButton = new Button();
             SuspendLayout();
+            // 
+            // emailTextBox
+            // 
+            emailTextBox.AccessibleName = "Email";
+            emailTextBox.Location = new Point(0, 0);
+            emailTextBox.Name = "emailTextBox";
+            emailTextBox.Size = new Size(100, 23);
+            emailTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            passwordTextBox.AccessibleName = "Password";
+            passwordTextBox.Location = new Point(8, 8);
+            passwordTextBox.Name = "passwordTextBox";
+            passwordTextBox.Size = new Size(100, 23);
+            passwordTextBox.TabIndex = 1;
+            passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(16, 16);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(75, 23);
+            loginButton.TabIndex = 2;
+            loginButton.Text = "Login";
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += loginButton_Click;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(24, 24);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(75, 23);
+            cancelButton.TabIndex = 3;
+            cancelButton.Text = "Cancel";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(644, 450);
+            Controls.Add(cancelButton);
+            Controls.Add(loginButton);
+            Controls.Add(passwordTextBox);
+            Controls.Add(emailTextBox);
             Name = "LoginForm";
             Text = "Login";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox emailTextBox;
+        private TextBox passwordTextBox;
+        private Button loginButton;
+        private Button cancelButton;
     }
 }
--- ToLearn/Forms/Account/LoginForm.cs
+++ ToLearn/Forms/Account/LoginForm.cs
@@ -4,17 +4,37 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToLearn.Models.Account;
 
-namespace ToLearn.Forms.Account
+namespace ToLearn.Forms.Account;
+
+public partial class LoginForm : Form
 {
-    public partial class LoginForm : Form
+    public LoginForm()
     {
-        public LoginForm()
+        InitializeComponent();
+    }
+
+    private void cancelButton_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+
+    private void loginButton_Click(object sender, EventArgs e)
+    {
+        var request = new Request()
         {
-            InitializeComponent();
-        }
+            Email = emailTextBox.Text,
+            Password = passwordTextBox.Text
+        };
+        var client = new HttpClient();
+        var result = client.PostAsJsonAsync<Request>("https://localhost:7190/login?useCookies=false&useSessionCookies=false", request);
+        MessageBox.Show(result.Result.Content.ReadAsStringAsync().Result);
     }
 }
