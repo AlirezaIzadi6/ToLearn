06cf37cc7d19959ed50e8cd71ec83b74f9273936
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-05 13:33:32 +0430

    Create models for sending, receiving and saving account information

---
+++ ToLearn/Models/Account/Request.cs
@@ -0,0 +1,7 @@
+﻿namespace ToLearn.Models.Account;
+
+public class Request
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ ToLearn/Models/Account/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ToLearn.Models.Account;
+
+public class User
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+}
