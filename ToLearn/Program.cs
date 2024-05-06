8b8f1261f32014f54b337f3c5d88d05e34df2b05
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-06 14:43:36 +0430

    Create Config class to save and retrieve data

--- ToLearn/Program.cs
+++ ToLearn/Program.cs
@@ -1,3 +1,5 @@
+using ToLearn.Utils;
+
 namespace ToLearn
 {
     internal static class Program
@@ -8,6 +10,7 @@ namespace ToLearn
         [STAThread]
         static void Main()
         {
+            Config.Initialize();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
---
+++ ToLearn/Utils/Config.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace ToLearn.Utils;
+
+public class Config
+{
+    private static string appData = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+    private static string _configPath = appData + @"\ToLearn\config";
+
+    public static void Initialize()
+    {
+        if (!Directory.Exists(_configPath))
+        {
+            Directory.CreateDirectory(_configPath);
+        }
+    }
+
+    public static void SaveConfig<T>(T obj)
+    {
+        string filePath = $"{_configPath}\\{typeof(T).Name}.json";
+        string fileContent = JsonSerializer.Serialize<T>(obj);
+        File.WriteAllText(filePath, fileContent);
+    }
+
+    public static T GetConfig<T>()
+    {
+        string filePath = $"{_configPath}\\{typeof(T).Name}.json";
+        string fileContent = File.ReadAllText(filePath);
+        T obj = JsonSerializer.Deserialize<T>(fileContent);
+        return obj;
+    }
+}
