b8d2d77b18a391571fa2ea637b741272ce2a6302
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-17 18:38:04 +0430

    Create ShowUnitsForm and its controls

---
+++ ToLearn/Forms/Flashcards/ShowUnitsForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ToLearn.Forms.Flashcards
+{
+    partial class ShowUnitsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            showCardsButton = new Button();
+            editButton = new Button();
+            deleteButton = new Button();
+            createNewUnitButton = new Button();
+            closeButton = new Button();
+            unitsComboBox = new ComboBox();
+            SuspendLayout();
+            // 
+            // showCardsButton
+            // 
+            showCardsButton.Location = new Point(8, 8);
+            showCardsButton.Name = "showCardsButton";
+            showCardsButton.Size = new Size(75, 23);
+            showCardsButton.TabIndex = 1;
+            showCardsButton.Tag = "ShowCards";
+            showCardsButton.Text = "Show cards";
+            showCardsButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            editButton.Location = new Point(16, 16);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(75, 23);
+            editButton.TabIndex = 2;
+            editButton.Tag = "Edit";
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(24, 24);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(75, 23);
+            deleteButton.TabIndex = 3;
+            deleteButton.Tag = "Delete";
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // createNewUnitButton
+            // 
+            createNewUnitButton.Location = new Point(32, 32);
+            createNewUnitButton.Name = "createNewUnitButton";
+            createNewUnitButton.Size = new Size(75, 23);
+            createNewUnitButton.TabIndex = 4;
+            createNewUnitButton.Tag = "CreateNewUnit";
+            createNewUnitButton.Text = "Create a new unit";
+            createNewUnitButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            closeButton.Location = new Point(40, 40);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new Size(75, 23);
+            closeButton.TabIndex = 5;
+            closeButton.Tag = "Close";
+            closeButton.Text = "Close";
+            closeButton.UseVisualStyleBackColor = true;
+            // 
+            // unitsComboBox
+            // 
+            unitsComboBox.AccessibleName = "List of units";
+            unitsComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            unitsComboBox.FormattingEnabled = true;
+            unitsComboBox.Location = new Point(0, 0);
+            unitsComboBox.Name = "unitsComboBox";
+            unitsComboBox.Size = new Size(121, 23);
+            unitsComboBox.TabIndex = 0;
+            unitsComboBox.Tag = "Units";
+            // 
+            // ShowUnitsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(644, 450);
+            Controls.Add(unitsComboBox);
+            Controls.Add(closeButton);
+            Controls.Add(createNewUnitButton);
+            Controls.Add(deleteButton);
+            Controls.Add(editButton);
+            Controls.Add(showCardsButton);
+            Name = "ShowUnitsForm";
+            Text = "Show Unit";
+            Load += ShowUnitsForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button showCardsButton;
+        private Button editButton;
+        private Button deleteButton;
+        private Button createNewUnitButton;
+        private Button closeButton;
+        private ComboBox unitsComboBox;
+    }
+}
---
+++ ToLearn/Forms/Flashcards/ShowUnitsForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToLearn.Models.Flashcards;
+using ToLearn.Utils;
+
+namespace ToLearn.Forms.Flashcards;
+
+public partial class ShowUnitsForm : CustomForm
+{
+    private readonly FlashcardsManager _flashcardsManager;
+    private readonly Deck _deck;
+
+    public ShowUnitsForm(Deck deck)
+    {
+        InitializeComponent();
+        _flashcardsManager = new FlashcardsManager(this);
+        _deck = deck;
+    }
+
+    private async void ShowUnitsForm_Load(object sender, EventArgs e)
+    {
+        await _flashcardsManager.ShowUnits(_deck);
+    }
+}
